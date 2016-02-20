1e8173ea19545d85770ec487ef5a1c4ba2b68fa2
Brent Laster <bl2@nclasters.org>
2016-02-20 10:08:25 -0500

    add in sample tests and jacoco configuration

---
+++ api/src/test/java/TestExample1.java
@@ -0,0 +1,14 @@
+// File: src/test/java/TestExample1.java
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestExample1 {
+
+  @Test public void example1() {
+
+	Assert.assertEquals("Testing with Gradle is easy", "Testing with Gradle is easy");
+  }
+}
+
+ 
